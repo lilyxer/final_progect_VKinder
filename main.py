4e8a6b7d86b6197c81790a5c9ec9eb711240f6df
Mikhail Borisov <borisov.uk@gmail.com>
2023-05-27 17:14:07 +0300

    создание класса бота

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-from vk_api.longpoll import VkLongPoll, VkEventType
 from models.VkBot import VkBot
-
-
-# def write_msg(user_id, message):
-#     authorize.method('messages.send', {'user_id': user_id, 'message': message,
-#                                        'random_id': get_random_id()})
-
-# # авторизуемся
-# authorize = vk_api.VkApi(token=config.GROUP_TOKEN)
-# user_access = vk_api.VkApi(token=config.CLIENT_TOKEN)
-
-# # получаем доступ к событиям
-# longpoll = VkLongPoll(authorize)
-# vk = authorize.get_api() 
-# # user_vk = user_access.get_api()
 
 if __name__ == '__main__':
     vk_bot = VkBot()
-    # Прослушивание сервера на событие
-    print('ckeiftv')
-    for event in vk_bot.longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.to_me:
-                msg = event.text.lower()
-                sender = event.user_id
-                vk_bot.write_msg(user_id=sender, message=msg)
+
--- models/VkBot.py
+++ models/VkBot.py
@@ -20,23 +20,33 @@ class VkBot:
         print('запустли')
         self.do_authauthorize()
         if self.authorize:
-            self.longpoll = VkLongPoll(self.authorize)
-            print('прошли')
+            self.session = self.authorize.get_api()
+            print('[+] Авторизация успешна')
+            print('[+] Создана сессия')
+            self.run_bot()
 
-
-    def do_authauthorize(self):
+    def do_authauthorize(self) -> None:
         """Проходим авторизацию
         создаём сессию, для работы с ботом
         """
         try:
             self.authorize = vk_api.VkApi(token=env('GROUP_TOKEN'))
-            self.session = self.authorize.get_api()
         except Exception as err:
             print(err, 'Авторизация завершилась неудачно', sep='\n')
-    
+
     def write_msg(self, user_id: int, message: str):
         """Автоответчик
         """
         self.authorize.method('messages.send', {'user_id': user_id, 
                                                 'message': message,
                                                 'random_id': get_random_id()})
+
+    def run_bot(self):
+        print('[+] Бот запущен')
+        self.longpoll = VkLongPoll(self.authorize)
+        for event in self.longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                if event.to_me:
+                    msg = event.text.lower()
+                    sender = event.user_id
+                    self.write_msg(user_id=sender, message=msg)
